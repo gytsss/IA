5ac419d06278bbc6ff634e6dadf0513cf23b6af8
Your Name <totogodoy@hotmail.com>
2024-08-22 20:09:37 -0300

    Add pathfinder package

---
+++ Assets/Pathfinder/INode.cs
@@ -0,0 +1,10 @@
+﻿public interface INode
+{
+    public bool IsBloqued();
+}
+
+public interface INode<Coordinate> 
+{
+    public void SetCoordinate(Coordinate coordinateType);
+    public Coordinate GetCoordinate();
+}
