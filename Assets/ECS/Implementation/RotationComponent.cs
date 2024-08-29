4a7dc85a6dff5ae919048a303d05cb91abfb3ee3
tobias <totogodoy@hotmail.com>
2024-08-29 20:04:42 -0300

    Add ECS movement and rotation systems

---
+++ Assets/ECS/Implementation/RotationComponent.cs
@@ -0,0 +1,16 @@
+﻿namespace ECS.Implementation
+{
+    public class RotationComponent : ECSComponent
+    {
+        public float X;
+        public float Y;
+        public float Z;
+
+        public RotationComponent(float X, float Y, float Z) 
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Z = Z;
+        }
+    }
+}
---
+++ Assets/ECS/Implementation/RotationSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ECS.Implementation;
+
+public sealed class RotationSystem : ECSSystem
+{
+    private ParallelOptions parallelOptions;
+    
+    private IDictionary<uint, RotationComponent> rotationComponents;
+    private IDictionary<uint, VelocityRotComponent> velocityRotComponents;
+    private IEnumerable<uint> queryedEntities;
+
+    public override void Initialize()
+    {
+        parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = 32 };
+    }
+
+    protected override void PreExecute(float deltaTime)
+    {
+        rotationComponents??= ECSManager.GetComponents<RotationComponent>();
+        velocityRotComponents??= ECSManager.GetComponents<VelocityRotComponent>();
+        queryedEntities??= ECSManager.GetEntitiesWhitComponentTypes(typeof(RotationComponent), typeof(VelocityRotComponent));
+    }
+
+    protected override void Execute(float deltaTime)
+    {
+        Parallel.ForEach(queryedEntities, parallelOptions, i =>
+        {
+            rotationComponents[i].X += velocityRotComponents[i].directionX * velocityRotComponents[i].velocity * deltaTime;
+            rotationComponents[i].Y += velocityRotComponents[i].directionY * velocityRotComponents[i].velocity * deltaTime;
+            rotationComponents[i].Z += velocityRotComponents[i].directionZ * velocityRotComponents[i].velocity * deltaTime;
+        });
+    }
+
+    protected override void PostExecute(float deltaTime)
+    {
+    }
+}
