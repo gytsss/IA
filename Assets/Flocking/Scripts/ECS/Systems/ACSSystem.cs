208712db89c5ee9b6b001af23aaada02095ac188
tobias <totogodoy@hotmail.com>
2024-09-05 20:17:14 -0300

    Add ECS systems and components

---
+++ Assets/Flocking/Scripts/ECS/Systems/ACSSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ECS.Implementation;
+
+public sealed class ACSSystem : ECSSystem
+{
+    private ParallelOptions parallelOptions;
+    
+    private IDictionary<uint, FlockingComponent> flockingComponents;
+    private IEnumerable<uint> queryedEntities;
+
+    public override void Initialize()
+    {
+        parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = 32 };
+    }
+
+    protected override void PreExecute(float deltaTime)
+    {
+        flockingComponents??= ECSManager.GetComponents<FlockingComponent>();
+        queryedEntities??= ECSManager.GetEntitiesWhitComponentTypes(typeof(FlockingComponent));
+    }
+
+    protected override void Execute(float deltaTime)
+    {
+        Parallel.ForEach(queryedEntities, parallelOptions, i =>
+        {
+             
+        });
+    }
+
+    protected override void PostExecute(float deltaTime)
+    {
+    }
+}
